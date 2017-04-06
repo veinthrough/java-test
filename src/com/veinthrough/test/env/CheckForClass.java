b84149d7c21792a2f961306d56f5255f6a01bc6b
veinthrough <veinthrough@gmail.com>
2017-04-06 18:19:05 +0800

    Add comments to class CheckForClass.

--- src/com/veinthrough/test/env/CheckForClass.java
+++ src/com/veinthrough/test/env/CheckForClass.java
@@ -2,6 +2,15 @@ package com.veinthrough.test.env;
 
 import com.veinthrough.test.AbstractUnitTester;
 
+/**
+ * Test for presence of class named in argv[0] on classpath at runtime.
+ * This uses Reflection (see chapter "Introspection, or a Class named Class")
+ * to find out if the given class (named on the command line) is present
+ * in the user's classpath at runtime. The class should not be "import"ed
+ * because we don't want to check it at compile time, only at runtime.
+ *
+ * @author veinthrough
+ */
 public class CheckForClass extends AbstractUnitTester {
     @Override
     public void test() {
